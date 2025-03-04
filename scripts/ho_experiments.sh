d433b90fc5478ff079dbfb55c7d024b0b258911b
jorge garcia <jorgarcia@us.es>
2025-03-04 21:57:12 +0100

    Minor changes

--- scripts/ho_experiments.sh
+++ scripts/ho_experiments.sh
@@ -2,3 +2,8 @@ $PWD/scripts/ho_trainer.sh cptac pam50 /media/jorge/SP_PHD_U3/perfil_molecular/f
 $PWD/scripts/ho_trainer.sh cptac er /media/jorge/SP_PHD_U3/perfil_molecular/features
 $PWD/scripts/ho_trainer.sh cptac pr /media/jorge/SP_PHD_U3/perfil_molecular/features
 $PWD/scripts/ho_trainer.sh cptac erbb2 /media/jorge/SP_PHD_U3/perfil_molecular/features
+
+$PWD/scripts/ho_trainer.sh tcga pam50 /media/jorge/SP_PHD_U3/perfil_molecular/features
+$PWD/scripts/ho_trainer.sh tcga er /media/jorge/SP_PHD_U3/perfil_molecular/features
+$PWD/scripts/ho_trainer.sh tcga pr /media/jorge/SP_PHD_U3/perfil_molecular/features
+$PWD/scripts/ho_trainer.sh tcga erbb2 /media/jorge/SP_PHD_U3/perfil_molecular/features
