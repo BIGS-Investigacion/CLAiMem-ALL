6cc6e0e7bbb935b2904dda89a179a798abe9fc15
jorge garcia <jorgarcia@us.es>
2025-03-13 14:42:57 +0100

    Scritps for I3US Cluster

---
+++ scripts/cv_experiments_i3us.sh
@@ -0,0 +1,19 @@
+$PWD/scripts/cv_trainer.sh cptac pam50 /home/jorgarcia/CLAiMem-ALL/.features YES 10
+$PWD/scripts/cv_trainer.sh cptac er /home/jorgarcia/CLAiMem-ALL/.features YES 10
+$PWD/scripts/cv_trainer.sh cptac pr /home/jorgarcia/CLAiMem-ALL/.features YES 10 
+$PWD/scripts/cv_trainer.sh cptac erbb2 /home/jorgarcia/CLAiMem-ALL/.features YES 10 
+
+$PWD/scripts/cv_trainer.sh tcga pam50 /home/jorgarcia/CLAiMem-ALL/.features YES 10
+$PWD/scripts/cv_trainer.sh tcga er /home/jorgarcia/CLAiMem-ALL/.features YES 10
+$PWD/scripts/cv_trainer.sh tcga pr /home/jorgarcia/CLAiMem-ALL/.features YES 10
+$PWD/scripts/cv_trainer.sh tcga erbb2 /home/jorgarcia/CLAiMem-ALL/.features YES 10
+
+$PWD/scripts/cv_trainer.sh cptac pam50 /home/jorgarcia/CLAiMem-ALL/.features NO 10
+$PWD/scripts/cv_trainer.sh cptac er /home/jorgarcia/CLAiMem-ALL/.features NO 10
+$PWD/scripts/cv_trainer.sh cptac pr /home/jorgarcia/CLAiMem-ALL/.features NO 10 
+$PWD/scripts/cv_trainer.sh cptac erbb2 /home/jorgarcia/CLAiMem-ALL/.features NO 10 
+
+$PWD/scripts/cv_trainer.sh tcga pam50 /home/jorgarcia/CLAiMem-ALL/.features NO 10
+$PWD/scripts/cv_trainer.sh tcga er /home/jorgarcia/CLAiMem-ALL/.features NO 10
+$PWD/scripts/cv_trainer.sh tcga pr /home/jorgarcia/CLAiMem-ALL/.features NO 10
+$PWD/scripts/cv_trainer.sh tcga erbb2 /home/jorgarcia/CLAiMem-ALL/.features NO 10
---
+++ scripts/ho_experiments_i3us.sh
@@ -0,0 +1,9 @@
+$PWD/scripts/ho_trainer.sh cptac pam50 /home/jorgarcia/CLAiMem-ALL/.features YES
+$PWD/scripts/ho_trainer.sh cptac er /home/jorgarcia/CLAiMem-ALL/.features YES
+$PWD/scripts/ho_trainer.sh cptac pr /home/jorgarcia/CLAiMem-ALL/.features YES
+$PWD/scripts/ho_trainer.sh cptac erbb2 /home/jorgarcia/CLAiMem-ALL/.features YES
+
+$PWD/scripts/ho_trainer.sh tcga pam50 /home/jorgarcia/CLAiMem-ALL/.features YES
+$PWD/scripts/ho_trainer.sh tcga er /home/jorgarcia/CLAiMem-ALL/.features YES
+$PWD/scripts/ho_trainer.sh tcga pr /home/jorgarcia/CLAiMem-ALL/.features YES
+$PWD/scripts/ho_trainer.sh tcga erbb2 /home/jorgarcia/CLAiMem-ALL/.features YES
