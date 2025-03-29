2a2ee583b0edbd2f9d0fa1e1c283e60451cf2aef
jorge garcia <jorgarcia@us.es>
2025-03-29 20:19:58 +0100

    Undoing fast changes...

--- src/bigs_auxiliar/filterer.py
+++ src/bigs_auxiliar/filterer.py
@@ -80,47 +80,43 @@ WSI_EXTENSION = args.wsi_extension
 WSI_DIR = args.wsi_dir
 PATCHES_DIR = args.patches_dir
 
-found = False
+
 #RECORREMOS LOS PATCHES DE CADA WSI
 for wsi in os.listdir(WSI_DIR):
-    if found:
-        if wsi.endswith(WSI_EXTENSION):
-            wsi_path = os.path.join(WSI_DIR, wsi)
+    if wsi.endswith(WSI_EXTENSION):
+        wsi_path = os.path.join(WSI_DIR, wsi)
+        
+        print(f"Procesando '{wsi}'...")
+
+        h5_input_path = os.path.join(PATCHES_DIR, wsi.replace(WSI_EXTENSION, '.h5'))
+        if not os.path.exists(h5_input_path):
+            print(f"El archivo {h5_input_path} no existe. Saltando '{wsi}'.")
+        else:
+            # === CARGAR COORDENADAS ===
+            with h5py.File(h5_input_path, 'r') as f:
+                coords = f['coords'][:]
+
+                # === ABRIR WSI ===
+                slide = openslide.OpenSlide(wsi_path)
+
+                # === LISTA DE COORDENADAS FILTRADAS ===
+                filtered_coords = []
+
+                # === CLASIFICAR PARCHES ===
+                for (x, y) in tqdm(coords, desc="Filtrando parches tumorales"):
+                    patch = slide.read_region((int(x), int(y)), 0, (PATCH_SIZE, PATCH_SIZE)).convert("RGB")
+
+                    input_tensor = transform(patch).unsqueeze(0).to(device)  # [1, 3, 224, 224]
+                    with torch.no_grad():
+                        outputs = model(input_tensor)
+                        pred = torch.argmax(outputs, dim=1).item()
+                    
+                        if pred == 1:
+                            filtered_coords.append([x, y])
+
+            print(str(len(filtered_coords)) + " out of " + str(len(coords)) + " patches are tumor")
+
             
-            print(f"Procesando '{wsi}'...")
-
-            h5_input_path = os.path.join(PATCHES_DIR, wsi.replace(WSI_EXTENSION, '.h5'))
-            if not os.path.exists(h5_input_path):
-                print(f"El archivo {h5_input_path} no existe. Saltando '{wsi}'.")
-            else:
-                # === CARGAR COORDENADAS ===
-                with h5py.File(h5_input_path, 'r') as f:
-                    coords = f['coords'][:]
-
-                    # === ABRIR WSI ===
-                    slide = openslide.OpenSlide(wsi_path)
-
-                    # === LISTA DE COORDENADAS FILTRADAS ===
-                    filtered_coords = []
-
-                    # === CLASIFICAR PARCHES ===
-                    for (x, y) in tqdm(coords, desc="Filtrando parches tumorales"):
-                        patch = slide.read_region((int(x), int(y)), 0, (PATCH_SIZE, PATCH_SIZE)).convert("RGB")
-
-                        input_tensor = transform(patch).unsqueeze(0).to(device)  # [1, 3, 224, 224]
-                        with torch.no_grad():
-                            outputs = model(input_tensor)
-                            pred = torch.argmax(outputs, dim=1).item()
-                        
-                            if pred == 1:
-                                filtered_coords.append([x, y])
-
-                print(str(len(filtered_coords)) + " out of " + str(len(coords)) + " patches are tumor")
-
-                
-                # === GUARDAR NUEVO ARCHIVO H5 ===
-                with h5py.File(h5_input_path, 'w') as f:
-                    f.create_dataset('coords', data=np.array(filtered_coords, dtype=int))
-    else:
-        if wsi.contains("TCGA-A8-A06U-01A-01-TS1.63824040-373f-4c6c-a74e-881c127567a6"):
-            found = True
+            # === GUARDAR NUEVO ARCHIVO H5 ===
+            with h5py.File(h5_input_path, 'w') as f:
+                f.create_dataset('coords', data=np.array(filtered_coords, dtype=int))
