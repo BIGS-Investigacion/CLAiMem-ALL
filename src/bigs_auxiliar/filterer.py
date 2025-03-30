33637ffba7247faec5755467e6c4b46e034d30d3
jorge garcia <jorgarcia@us.es>
2025-03-30 10:16:50 +0200

    Changes to optimize tumour filtering

--- src/bigs_auxiliar/filterer.py
+++ src/bigs_auxiliar/filterer.py
@@ -11,6 +11,25 @@ from torchvision.models import vgg16
 from huggingface_hub import hf_hub_download
 import torch
 
+def load_WSI_names(csv_path:str, suffix:str) -> list[str]:
+    """
+    Load WSI names from a CSV file.
+
+    Args:
+        csv_path (str): Path to the CSV file.
+
+    Returns:
+        list: List of WSI names.
+    """
+
+    wsi_names = []
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            if row:  # Ensure the row is not empty
+                wsi_names.append(row[1]+suffix)  # Assuming WSI names are in the first column
+    return wsi_names
+
 class ModifiedVGG16(nn.Module):
     def __init__(self):
         super().__init__()
@@ -35,6 +54,7 @@ class ModifiedVGG16(nn.Module):
 
 
 from torchvision import transforms
+import csv
 
 # === Transformación basada en el JSON del modelo ===
 transform = transforms.Compose([
@@ -71,7 +91,7 @@ parser.add_argument('--patch_size', type=int, required=True, help="Patch size (e
 parser.add_argument('--wsi_extension', type=str, required=True, help="WSI file extension (e.g., '.svs').")
 parser.add_argument('--wsi_dir', type=str, required=True, help="Directory containing WSI files.")
 parser.add_argument('--patches_dir', type=str, required=True, help="Directory containing patch files.")
-
+parser.add_argument('--csv_list', type=str, required=True, help="CSV to filter WSIs with information.")
 args = parser.parse_args()
 
 DATABASE = args.database
@@ -79,11 +99,13 @@ PATCH_SIZE = args.patch_size
 WSI_EXTENSION = args.wsi_extension
 WSI_DIR = args.wsi_dir
 PATCHES_DIR = args.patches_dir
+WSI_LIST = load_WSI_names(args.csv_list, WSI_EXTENSION)
+
 
 
 #RECORREMOS LOS PATCHES DE CADA WSI
-for wsi in os.listdir(WSI_DIR):
-    if wsi.endswith(WSI_EXTENSION):
+for wsi in sorted(os.listdir(WSI_DIR)):
+    if wsi in WSI_LIST:
         wsi_path = os.path.join(WSI_DIR, wsi)
         
         print(f"Procesando '{wsi}'...")
@@ -120,3 +142,6 @@ for wsi in os.listdir(WSI_DIR):
             # === GUARDAR NUEVO ARCHIVO H5 ===
             with h5py.File(h5_input_path, 'w') as f:
                 f.create_dataset('coords', data=np.array(filtered_coords, dtype=int))
+    else:
+        print(f"'{wsi}' no está en la lista. Borrando archivo...")
+        os.remove(os.path.join(WSI_DIR, wsi))
