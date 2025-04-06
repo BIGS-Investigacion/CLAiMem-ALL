648b8bbcbf243d85e5e2ab2c63088a6d5b638ffe
jorge garcia <jorgarcia@us.es>
2025-04-06 21:09:05 +0200

    Changes to evaluate hold-out and cv

---
+++ scripts/ev_experiments.sh
@@ -0,0 +1,1 @@
+$PWD/scripts/evaluator.sh /media/jorge/SP_PHD_U3/perfil_molecular/features_20x 0
--- scripts/evaluator.sh
+++ scripts/evaluator.sh
@@ -1,88 +1,122 @@
 #!/bin/bash
-
-if [ "$1" != "cptac" ] && [ "$1" != "tcga" ]; then
-    echo "Invalid database name. Use 'cptac' or 'tgca'."
-    exit 1
-else
-    DATABASE=$1
-fi
-
-if [ "$2" == "pam50" ]; then
-    CSV_FILE=data/dataset_csv/$DATABASE-subtype_pam50.csv
-    LABEL_DICT="{'basal':0,'her2':1,'luma':2,'lumb':3,'normal':4}"
-elif [ "$2" == "ihc" ]; then
-    CSV_FILE=data/dataset_csv/$DATABASE-subtype_ihc.csv
-    LABEL_DICT="{'Triple-negative':0,'Luminal-A':1,'Her2-not-luminal':2,'Luminal-B(HER2-)':3,'Luminal-B(HER2+)':4}"
-elif [ "$2" == "ihc_simple" ]; then
-    CSV_FILE=data/dataset_csv/$DATABASE-subtype_ihc_simple.csv
-    LABEL_DICT="{'Triple-negative':0,'Luminal-A':1,'Luminal-B':2,'HER2':3}"
-elif [ "$2" == "erbb2" ]; then
-    CSV_FILE=data/dataset_csv/$DATABASE-erbb2.csv
-    LABEL_DICT="{'negative':0,'positive':1}"
-elif [ "$2" == "pr" ]; then
-    CSV_FILE=data/dataset_csv/$DATABASE-pr.csv
-    LABEL_DICT="{'negative':0,'positive':1}"
-elif [ "$2" == "er" ]; then
-    CSV_FILE=data/dataset_csv/$DATABASE-er.csv
-    LABEL_DICT="{'negative':0,'positive':1}"
-else
-    echo "Invalid parameter. Use 'ihc', 'ihc_simple', 'pam50', 'er', 'pr' or 'erbb2'."
-    exit 1
-fi
-
-if [ -z "$3" ]; then
+if [ -z "$1" ]; then
     echo "Please provide the directory where the features are located."
     exit 1
 else
-    F_DIRECTORY=$3
+    F_DIRECTORY=$1
 fi
 
-if ! [[ "$4" =~ ^[0-9]+$ ]] || [ "$4" -lt 0 ]; then
+if ! [[ "$2" =~ ^[0-9]+$ ]] || [ "$2" -lt 0 ]; then
     echo "Please provide a non-negative integer for the cross-validation parameter. 0 means hold-out"
     exit 1
-elif [ "$4" -eq 0 ]; then
+elif [ "$2" -eq 0 ]; then
     K=1
     VALIDATION="ho"
 else
-    K=$4
-    VALIDATION=$4-"cv"
+    K=$2
+    VALIDATION=$2-"cv"
 fi
 
-CLAM_MODEL_TYPE=clam_sb
-MODEL_SIZE=big
-DROP_OUT=0.25
-CUDA_DEV=0
 
-for dir in .results/$DATABASE/$2/$CLAM_MODEL_TYPE/$VALIDATION*; do
-    if [ -d "$dir" ]; then
-        RESULTS_DIR=$dir
-        for subdir in $RESULTS_DIR/*/; do
-            
-            if [ -d "$subdir" ]; then
-                MODEL_NAME=$(basename "$subdir")
-                # Add your processing commands here
-                if [[ "$MODEL_NAME" == "conch" ]]; then
-                    EMBED_DIM=512
-                elif [[ "$MODEL_NAME" == "ctranspath" ]] || [[ "$MODEL_NAME" == "hibou_b" ]]; then
-                    EMBED_DIM=768
-                elif [[ "$MODEL_NAME" == "hoptimus0" ]] || [[ "$MODEL_NAME" == "provgigapath" ]] || [[ "$MODEL_NAME" == "uni_2" ]]; then
-                    EMBED_DIM=1536
-                elif [[ "$MODEL_NAME" == "musk" ]] || [[ "$MODEL_NAME" == "retccl" ]]; then
-                    EMBED_DIM=2048
-                elif [[ "$MODEL_NAME" == "virchow" ]]; then
-                    EMBED_DIM=2560
-                else
-                    EMBED_DIM=1024
-                fi
-                FEATURES_DIRECTORY=$F_DIRECTORY/$DATABASE/features_$MODEL_NAME
-                EXP_CODE=$MODEL_NAME
-                EXP_CODE_SAVE=$(echo $dir | tr '/' '-')-$MODEL_NAME-summary
-                CUDA_VISIBLE_DEVICES=$CUDA_DEV  python src/eval.py  --drop_out $DROP_OUT --model_size $MODEL_SIZE --k $K --models_exp_code $EXP_CODE --save_exp_code $EXP_CODE_SAVE --csv_path $CSV_FILE --label_dict $LABEL_DICT --model_type $CLAM_MODEL_TYPE --results_dir $RESULTS_DIR --data_root_dir $FEATURES_DIRECTORY --embed_dim $EMBED_DIM
-                #CUDA_VISIBLE_DEVICES=0 src/python create_heatmaps.py --config config_template.yaml
+
+MODEL_SIZE=small
+DROP_OUT=0.7
+CUDA_DEV=0
+RESULTS_DIR=.results
+
+for dir_database in $RESULTS_DIR/*; do
+    if [ -d "$dir_database" ]; then
+        DATABASE=$(basename $dir_database)
+        for aim in $dir_database/*; do
+            if [[ $aim == *"pam50" ]]; then
+                CSV_FILE=data/dataset_csv/$DATABASE-subtype_pam50.csv
+                LABEL_DICT="{'basal':0,'her2':1,'luma':2,'lumb':3,'normal':4}"
+                CLAM_MODEL_TYPE=clam_mb
+            elif [[ $aim == *"ihc" ]]; then
+                CSV_FILE=data/dataset_csv/$DATABASE-subtype_ihc.csv
+                LABEL_DICT="{'Triple-negative':0,'Luminal-A':1,'Her2-not-luminal':2,'Luminal-B(HER2-)':3,'Luminal-B(HER2+)':4}"
+                CLAM_MODEL_TYPE=clam_mb
+            elif [[ $aim == *"ihc_simple" ]]; then
+                CSV_FILE=data/dataset_csv/$DATABASE-subtype_ihc_simple.csv
+                LABEL_DICT="{'Triple-negative':0,'Luminal-A':1,'Luminal-B':2,'HER2':3}"
+                CLAM_MODEL_TYPE=clam_mb
+            elif [[ $aim == *"erbb2" ]]; then
+                CSV_FILE=data/dataset_csv/$DATABASE-erbb2.csv
+                LABEL_DICT="{'negative':0,'positive':1}"
+                CLAM_MODEL_TYPE=clam_sb
+            elif [[ $aim == *"pr" ]]; then
+                CSV_FILE=data/dataset_csv/$DATABASE-pr.csv
+                LABEL_DICT="{'negative':0,'positive':1}"
+                CLAM_MODEL_TYPE=clam_sb
+            elif [[ $aim == *"er" ]]; then
+                CSV_FILE=data/dataset_csv/$DATABASE-er.csv
+                LABEL_DICT="{'negative':0,'positive':1}"
+                CLAM_MODEL_TYPE=clam_sb
+            else
+                echo "Invalid parameter. Use 'ihc', 'ihc_simple', 'pam50', 'er', 'pr' or 'erbb2'."
+                exit 1
             fi
+            for test in $aim/$CLAM_MODEL_TYPE/$VALIDATION*/*; do
+                valid=True
+                if [[ $test == *"cnn"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=cnn
+                elif [[ $test == *"conch"* ]]; then
+                    EMBED_DIM=512
+                    MODEL_NAME=conch
+                elif [[ $test == *"ctranspath"* ]]; then
+                    EMBED_DIM=768
+                    MODEL_NAME=ctranspath
+                elif [[ $test == *"hibou_b"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=hibou_b
+                elif [[ $test == *"hibou_l"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=hibou_l
+                elif [[ $test == *"hoptimus0"* ]]; then
+                    EMBED_DIM=1536
+                    MODEL_NAME=hoptimus0
+                elif [[ $test == *"provgigapath"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=provgigapath
+                elif [[ $test == *"phikon"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=phikon
+                elif [[ $test == *"uni"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=uni
+                elif [[ $test == *"uni_2"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=uni_2
+                elif [[ $test == *"musk"* ]]; then
+                    EMBED_DIM=2048
+                    MODEL_NAME=musk
+                elif [[ $test == *"retccl"* ]]; then
+                    EMBED_DIM=1024
+                    MODEL_NAME=retccl
+                elif [[ $test == *"virchow"* ]]; then
+                    EMBED_DIM=2560
+                    MODEL_NAME=virchow
+                else
+                    echo $test
+                    echo "Invalid parameter. Use 'conch', 'ctranspath', 'hibou_b', 'hoptimus0', 'provgigapath', 'uni_2', 'musk', 'retccl' or 'virchow'."
+                    valid=False
+                fi
+                if [ "$valid" = True ]; then
+                    FEATURES_DIRECTORY=$F_DIRECTORY/$DATABASE/features_$MODEL_NAME
+                    echo $FEATURES_DIRECTORY
+                    EXP_CODE=$MODEL_NAME
+                    EXP_CODE_SAVE=$(echo $test | tr '/' '-')-$MODEL_NAME-summary
+                    CUDA_VISIBLE_DEVICES=$CUDA_DEV  python src/eval.py  --drop_out $DROP_OUT --model_size $MODEL_SIZE --k $K --models_exp_code $EXP_CODE --save_exp_code $EXP_CODE_SAVE --csv_path $CSV_FILE --label_dict $LABEL_DICT --model_type $CLAM_MODEL_TYPE --results_dir $test --data_root_dir $FEATURES_DIRECTORY --embed_dim $EMBED_DIM
+                    #CUDA_VISIBLE_DEVICES=0 src/python create_heatmaps.py --config config_template.yaml
+                fi
+            done
         done
     fi
 done
+
+
+
 
 
 
