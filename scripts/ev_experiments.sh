e26e9e4c7c8311c42c3cb3139e30b1f4b5a86d4b
jorge garcia <jorgarcia@us.es>
2025-04-22 13:30:34 +0200

    Changes to generalize script

--- scripts/ev_experiments.sh
+++ scripts/ev_experiments.sh
@@ -1,2 +1,1 @@
-$PWD/scripts/evaluator.sh /media/jorge/SP_PHD_U3/perfil_molecular/features_20x 0
-$PWD/scripts/evaluator.sh /media/jorge/SP_PHD_U3/perfil_molecular/features_20x 10
+$PWD/scripts/evaluator.sh $1 $2
