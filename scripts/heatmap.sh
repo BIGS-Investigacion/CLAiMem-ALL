1067877264561e0bfb7d5f5be0592f7a130a3f3a
jorge garcia <jorgarcia@us.es>
2025-05-30 12:23:22 +0200

    Changes to work with the rest of  tasks

--- scripts/heatmap.sh
+++ scripts/heatmap.sh
@@ -1,2 +1,2 @@
 export CONCH_CKPT_PATH='.checkpoint/conch/pytorch_model.bin'
-CUDA_VISIBLE_DEVICES=0 python src/create_heatmaps.py --config config/heatmaps/pam50.yaml
+CUDA_VISIBLE_DEVICES=0 python src/create_heatmaps.py --config $1
