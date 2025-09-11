8e5c98dc03e16c7adf6ca0fb30f86db8446171ba
jorge garcia <jorgarcia@us.es>
2025-09-11 13:23:47 +0200

    Config update

--- scripts/he2her2.sh
+++ scripts/he2her2.sh
@@ -1,4 +1,4 @@
 python src/he2her2.py --data_root ../../datos/BCI_dataset --out_dir runs/he2her2_pix2pix \
-    --image_size 1024 --batch_size 4 --epochs 200
+    --image_size 512 --batch_size 32 --epochs 200 --resume 
 python src/he2her2.py --mode infer --data_root ../../datos/BCI_dataset \
-    --weights runs/he2her2_pix2pix/last.pt --out_dir runs/he2her2_pix2pix --image_size 1024
+    --weights runs/he2her2_pix2pix/G_final.pt --out_dir runs/he2her2_pix2pix --image_size 512
