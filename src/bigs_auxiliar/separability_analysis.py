eac1b045116cf6c1de3948fb6342299562f1e1cc
jorge garcia <jorgarcia@us.es>
2025-11-03 20:05:08 +0100

    minor change

--- src/bigs_auxiliar/separability_analysis.py
+++ src/bigs_auxiliar/separability_analysis.py
@@ -3,7 +3,6 @@ import h5py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.metrics import silhouette_score, silhouette_samples
 from pathlib import Path
